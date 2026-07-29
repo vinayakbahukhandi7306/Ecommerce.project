afc9192620e41c0f77663872d6e9aed533509a13
Vinayak <Vinayakbahukhandi37@gmail.com>
2026-07-29 14:05:59 +0530

    User Registration Service with password Encryption

--- src/main/java/com/vinayak/ecommerce/service/impl/UserServiceImpl.java
+++ src/main/java/com/vinayak/ecommerce/service/impl/UserServiceImpl.java
@@ -1,14 +1,43 @@
 package com.vinayak.ecommerce.service.impl;
 
 import com.vinayak.ecommerce.dto.RegisterRequest;
+import com.vinayak.ecommerce.entity.User;
+import com.vinayak.ecommerce.enums.Role;
+import com.vinayak.ecommerce.repository.UserRepository;
 import com.vinayak.ecommerce.service.UserService;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
 public class UserServiceImpl implements UserService {
 
+    private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
+
+    public UserServiceImpl(UserRepository userRepository,
+                           PasswordEncoder passwordEncoder) {
+        this.userRepository = userRepository;
+        this.passwordEncoder = passwordEncoder;
+    }
+
     @Override
     public void register(RegisterRequest request) {
 
+        if (userRepository.existsByEmail(request.getEmail())) {
+            throw new RuntimeException("Email already exists");
+        }
+
+        User user = new User();
+
+        user.setFirstName(request.getFirstName());
+        user.setLastName(request.getLastName());
+        user.setEmail(request.getEmail());
+
+        // Encrypt password before saving
+        user.setPassword(passwordEncoder.encode(request.getPassword()));
+
+        user.setRole(Role.CUSTOMER);
+
+        userRepository.save(user);
     }
 }
