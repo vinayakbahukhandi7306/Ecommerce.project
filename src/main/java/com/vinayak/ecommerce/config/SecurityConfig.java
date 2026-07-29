6a44900318d3db6ced1bdeb004c71b3a57f8b5d5
Vinayak <Vinayakbahukhandi37@gmail.com>
2026-07-29 15:02:04 +0530

    Implement user registration REST APT

--- src/main/java/com/vinayak/ecommerce/config/SecurityConfig.java
+++ src/main/java/com/vinayak/ecommerce/config/SecurityConfig.java
@@ -2,8 +2,11 @@ package com.vinayak.ecommerce.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 public class SecurityConfig {
@@ -11,5 +14,19 @@ public class SecurityConfig {
     @Bean
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+
+        http
+                .csrf(csrf -> csrf.disable())
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers("/api/auth/**").permitAll()
+                        .anyRequest().authenticated()
+                )
+                .httpBasic(Customizer.withDefaults());
+
+        return http.build();
     }
 }
---
+++ src/main/java/com/vinayak/ecommerce/controller/AuthController.java
@@ -0,0 +1,26 @@
+package com.vinayak.ecommerce.controller;
+
+import com.vinayak.ecommerce.dto.RegisterRequest;
+import com.vinayak.ecommerce.service.UserService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/auth")
+public class AuthController {
+
+    private final UserService userService;
+
+    public AuthController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @PostMapping("/register")
+    public ResponseEntity<String> register(@RequestBody RegisterRequest request) {
+
+        userService.register(request);
+
+        return new ResponseEntity<>("User Registered Successfully", HttpStatus.CREATED);
+    }
+}
